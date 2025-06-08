9928bd349c420b9684435131346c09f4731f57ba
alejandro <camacho044@gmail.com>
2025-06-07 20:23:53 -0500

    first commit

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'side-menu',
+  imports: [],
+  templateUrl: './side-menu.component.html'
+})
+export class SideMenuComponent { }
