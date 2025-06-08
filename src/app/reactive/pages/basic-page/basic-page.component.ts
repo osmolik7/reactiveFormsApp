a29030b95c2ee6a9e917370efff7bca3e532c8b9
alejandro <camacho044@gmail.com>
2025-06-08 16:43:55 -0500

    forumulario reactivo con validaciones

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
 })
-export class BasicPageComponent { }
+export class BasicPageComponent { 
+  private fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]/** validadores sincronos */,  []/** validadores asincronos */],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  myForm2 = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  }); 
+}
