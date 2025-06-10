0a3f9e463d37326e8e45052bffa9acc4e1db7a48
alejandro <camacho044@gmail.com>
2025-06-09 20:47:35 -0500

    formularios dinamicos con validaciones

---
+++ src/app/utils/form-utils.ts
@@ -0,0 +1,44 @@
+import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+
+
+export class FormUtils {
+
+    static getTextError(errors: ValidationErrors): string | null{
+        for(const key of Object.keys(errors)){
+            switch(key){
+                case 'required':
+                return 'Este campo es requerido';
+                case 'minlength':
+                return `Minimo de ${ errors['minlength'].requiredLength } caracteres.`
+                case 'min':
+                return `Valor minimo de ${ errors['min'].min }`;
+            }
+        }
+        return null;
+    }
+
+    static isValidField(form:FormGroup, fieldName:string):boolean|null {
+        return (form.controls[fieldName].errors && form.controls[fieldName].touched);
+    }
+
+    static getFieldError(form:FormGroup, fieldName:string):string|null{
+        if(!form.controls[fieldName]) return null;
+        const errors = form.controls[fieldName].errors ?? {};
+        return this.getTextError(errors);
+
+    }
+
+    static isValidFieldInArray(formArray: FormArray, index:number){
+        return (
+        formArray.controls[index].errors && formArray.controls[index].touched
+        )
+    }
+
+    static getFieldErrorInArray(formArray:FormArray, index:number):string|null{
+        if(formArray.controls.length == 0) return null;
+        const errors = formArray.controls[index].errors ?? {};
+         return this.getTextError(errors);
+    }
+
+  
+}
